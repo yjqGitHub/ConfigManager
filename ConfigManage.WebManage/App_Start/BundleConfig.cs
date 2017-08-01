44b67a55cc225938c25f3b10c10ddd800b75c262
叶俊权 <425527169@qq.com>
2017-08-01 23:25:30 +0800

    change a litter

--- ConfigManage.WebManage/App_Start/BundleConfig.cs
+++ ConfigManage.WebManage/App_Start/BundleConfig.cs
@@ -15,12 +15,27 @@ namespace ConfigManage.WebManage
         public static void RegisterBundles(BundleCollection bundles)
         {
             BindCommonToolJs(bundles);
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-{version}.js"));
+            BindListJsAndCss(bundles);
+        }
 
-            //bundles.Add(new StyleBundle("~/Content/css").Include(
-            //          "~/Content/bootstrap.css",
-            //          "~/Content/site.css"));
+        private static void BindListJsAndCss(BundleCollection bundles)
+        {
+            //list母版页公共js
+            bundles.Add(new ScriptBundle("~/ListLayout/js").Include(
+                        "~/Content/lib/jquery/1.9.1/jquery.min.js",
+                        "~/Content/lib/layer/3.0.3/layer.js",
+                        "~/Content/static/h-ui/js/H-ui.min.js",
+                        "~/Content/static/h-ui.admin/js/H-ui.admin.js",
+                        "~/Content/JS/CommonJs/DialogJs.js",
+                        "~/Content/JS/CommonJs/ToolJs.js",
+                        "~/Content/JS/CommonJs/tableHeadFixer.js"));
+            //list母版页公共css
+            bundles.Add(new StyleBundle("~/ListLayout/css").Include(
+                      "~/Content/static/h-ui/css/H-ui.min.css",
+                      "~/Content/static/h-ui.admin/css/H-ui.admin.cs",
+                      "~/Content/lib/Hui-iconfont/1.0.8/iconfont.css",
+                      "~/Content/static/h-ui.admin/skin/blue/skin.css",
+                      "~/Content/static/h-ui.admin/css/style.css"));
         }
 
         private static void BindCommonToolJs(BundleCollection bundles)
