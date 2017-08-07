01158c6499a99f95bae23b66b0f1bf90c01aeb35
叶俊权 <425527169@qq.com>
2017-08-07 21:45:27 +0800

    补全未提交的

---
+++ ConfigManager.Constant/Constants/IgnoreFieldsConstant.cs
@@ -0,0 +1,17 @@
+﻿namespace ConfigManager.Constant.Constants
+{
+    /// <summary>
+    /// Copyright (C) 2017 yjq 版权所有。
+    /// 类名：IgnoreFieldsConstant.cs
+    /// 类属性：公共类（非静态）
+    /// 类功能描述：忽略键的常量
+    /// 创建标识：yjq 2017/8/6 21:45:37
+    /// </summary>
+    public class IgnoreFieldsConstant
+    {
+        /// <summary>
+        /// fid
+        /// </summary>
+        public static readonly string[] Ignore_FID = new string[1] { "FID" };
+    }
+}
---
+++ JQ.Web/Extensions/ModelStateExtension.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace JQ.Web.Extensions
+{
+    /// <summary>
+    /// Copyright (C) 2017 yjq 版权所有。
+    /// 类名：ModelStateExtension.cs
+    /// 类属性：公共类（非静态）
+    /// 类功能描述：ModelStateExtension
+    /// 创建标识：yjq 2017/8/6 21:55:51
+    /// </summary>
+    public static class ModelStateExtension
+    {
+        /// <summary>
+        /// 获取第一个错误信息
+        /// </summary>
+        /// <param name="modelState">绑定状态</param>
+        /// <returns>第一个错误信息</returns>
+        public static string GetFirstErrorMsg(this ModelStateDictionary modelState)
+        {
+            string errorMessage = string.Empty;
+            if (modelState != null && (!modelState.IsValid))
+            {
+                foreach (var item in modelState.Values)
+                {
+                    if (item.Errors != null && item.Errors.Count > 0)
+                    {
+                        errorMessage = item.Errors.FirstOrDefault()?.ErrorMessage;
+                        break;
+                    }
+                }
+            }
+            return errorMessage;
+        }
+    }
+}
